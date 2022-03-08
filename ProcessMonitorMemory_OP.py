fa67d384e283017e898a724f7e28c41efa404c7a
omkargodase <36079081+omkargodase@users.noreply.github.com>
2022-03-08 15:11:56 +0530

    PROCESS MEMORY
    This Code Display's Current Running Process On Local Machine With Memory Used

---
+++ ProcessMonitorMemory_OP.py
@@ -0,0 +1,53 @@
+##################################################################################
+# Required Python Modules
+##################################################################################
+
+
+import psutil
+
+##################################################################################
+# Function Name : ProcessDisplay
+# Description   : Display Current Running Process On Local Machine With Memory Used
+# Author        : Omkar Rajendra Godase
+# Date          : 17/02/2022  
+##################################################################################
+
+def ProcessDisplay():
+    listprocess = []
+
+    for Proc in psutil.process_iter():
+        try:
+            Pinfo = Proc.as_dict(attrs = ['pid','name','username'])
+            
+            Pinfo['vms'] = Proc.memory_info().vms / (1024 * 1024)
+            
+            listprocess.append(Pinfo)
+
+        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+            pass
+
+    return listprocess
+
+##################################################################################
+# Function Name : Main
+# Description   : Main Function From Where Execution Starts
+# Author        : Omkar Rajendra Godase
+# Date          : 07/01/2022  
+##################################################################################
+
+def main():
+    print("OMII GODASE : PYTHON AUTOMATION AND MACHINE LEARNING")
+
+    print("Process Monitor")
+
+    listprocess = ProcessDisplay()
+
+    for Elem in listprocess:
+        print(Elem)
+
+##################################################################################
+# Function Name : Starter Of Program
+##################################################################################
+
+if __name__ == "__main__":
+    main()
